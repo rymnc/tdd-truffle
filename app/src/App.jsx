406f33c6a9de109384a2db438e15250f2cb1a18b
Aaryamann Challani <aaryamannchallani7@gmail.com>
2020-06-28 22:10:13 +0530

    Refactored

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Web3ReactProvider } from "@web3-react/core";
 import Web3 from "web3";
-
 import "./bootstrap.min.css";
 import Wallet from "./components/Wallet";
+import EventCatcher from "./components/EventCatcher";
 
 function getLibrary(provider) {
   const library = new Web3(provider);
-
   library.pollingInterval = 12000;
-
+  console.log(library.givenProvider);
   return library;
 }
 
@@ -17,6 +16,7 @@ const App = () => {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <Wallet />
+      
     </Web3ReactProvider>
   );
 };
